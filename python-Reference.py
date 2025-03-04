9008640dd503d58fa7263a22fcd38f1485fe616e
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2025-03-05 00:01:33 +0600

    string methods commit

--- python-Reference.py
+++ python-Reference.py
@@ -31,8 +31,42 @@ class child(parent):
         return super().greet()+"and child"
 
 print(child().greet())
-"""
+
 
 a=[1,2,3]
 b=["a","b","c"]
 print(list(zip(a,b)))
+
+txt="my name is rif"
+
+print(txt.encode(encoding="ascii",errors="backslarshereolace"))
+
+
+
+txt="banana"
+x=txt.upper()#(20)
+print(x)
+
+mydict={83:80}
+txt="Hello Sam!"
+print(txt.translate(mydict))
+
+
+
+txt = "Good night Sam!"
+mydict = {109: 101, 83: 74, 97: 111, 111: None, 100: None, 110: None, 103: None, 104: None, 116: None}
+print(txt.translate(mydict))
+
+
+myTuple=("John","peter","Vicky")
+x="#".join(myTuple)
+print(x)
+
+
+txt="I could eat bananas all day "
+x=txt.partition(("bananas"))
+print(x)
+"""
+txt="THIS IS NOW!"
+x=txt.isupper()
+print(x)
