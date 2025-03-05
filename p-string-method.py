c116472d4cdf4fed03da1aa69c6a106922432fa2
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2025-03-05 22:55:18 +0600

    p string method commit

---
+++ p-string-method.py
@@ -0,0 +1,29 @@
+"""
+#fruits=['apple','banana','cherry']
+#fruits.append("orange")
+a= ["apple", "banana", "cherry"]
+b = ["Ford", "BMW", "Volvo"]
+print (a.append(b))
+#print(fruits)
+
+
+fruits = ['apple', 'banana', 'cherry', 'orange']
+fruits.clear()
+print(fruits)
+
+
+fruits=fruits = ['apple', 'banana', 'cherry', 'orange']
+x=fruits.copy()
+print(x)
+
+fruits = ['apple', 'banana', 'cherry', 'orange']
+x=fruits.count("cherry")
+print(x)
+"""
+
+fruits = ['apple', 'banana', 'cherry', 'orange']
+cars=['Ford','BMW','Volvo']
+fruits.extend(cars)
+print(fruits)
+
+
