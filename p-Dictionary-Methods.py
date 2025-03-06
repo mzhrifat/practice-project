8e3e0ce69b7ac2311e40ca163e67c0fe39e5ef51
MD.ZIAUL HASHAN RIFAT <153647130+mzhrifat@users.noreply.github.com>
2025-03-07 02:20:14 +0600

    Update p-Dictionary-Methods.py

--- p-Dictionary-Methods.py
+++ p-Dictionary-Methods.py
@@ -1,3 +1,4 @@
+"""
 car = {
   "brand": "Ford",
   "model": "Mustang",
@@ -7,3 +8,86 @@ car = {
 car.clear()
 
 print(car)
+
+car={
+  "brand":"Ford",
+  "model":"Mustang",
+  "year":1963
+}
+x=car.copy()
+print(x)
+
+x=('key1','key2','key3')
+y=0
+thisdict=dict.fromkeys(x,y)
+print(thisdict)
+
+
+car={
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+x=car.get("price",1500)
+print(x)
+
+
+
+car={
+  "brand":"Ford",
+  "model":"jj",
+  "year":1960
+}
+x=car.keys()
+print(x)
+"""
+car={
+  "brand":"Ford",
+  "model":"jj",
+  "year":1960
+}
+x=car.items()
+car["color"]="white"
+print(x)
+
+car={
+  "brand":"Ford",
+  "model":"Mustang",
+  "year":1964
+}
+
+car.pop("model")
+print(car)
+
+
+car = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+x = car.setdefault("model", "Bronco")
+
+print(x)
+
+
+
+car = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+x = car.update({"color": "white"})
+
+print(x)
+
+
+car = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+x=car.values()
+print(x)
