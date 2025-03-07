3e323c7c5d3d817783ac6fbb144bfee44e36a0f3
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2025-03-08 01:15:09 +0600

    union commit

--- set-methods.py
+++ set-methods.py
@@ -5,3 +5,75 @@ print(fruits)
 
 fruits={"apple","banana","cherry"}
 print(fruits)
+
+
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+z=x.intersection(y)
+print(z)
+
+
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+x.intersection_update(y)
+print(x)
+x = {"a", "b", "c"}
+y = {"c", "d", "e"}
+z = {"f", "g", "c"}
+
+x &= y & z
+
+print(x)
+
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+
+z=x.isdisjoint(y)
+print(z)
+
+
+x = {"a", "b", "c"}
+y = {"f", "e", "d", "c", "b", "a"}
+#z=x.issubset(y)
+z=x<=y
+print(z)
+
+
+x = {"a", "b", "c"}
+y = {"f", "e", "d", "c", "b", "a"}
+z=x.issuperset(y)
+print(z)
+
+
+fruits={"apple","banana","cherry"}
+#fruits.pop()
+x=fruits.pop()
+print(x)
+
+
+fruits={"apple","banana","cherry"}
+fruits.remove("banana")
+print(fruits)
+
+
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+#z=x.symmetric_difference(y)
+x.symmetric_difference_update(y)
+print(x)
+
+
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+z=x.union(y)
+print(z)
+
+
+x = {"a", "b", "c"}
+y = {"f", "d", "a"}
+z = {"c", "d", "e"}
+
+result = x.union(y, z)
+
+print(result)
+
