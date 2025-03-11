10fe9dda2abab8981249566c72a8da709074e6ed
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2025-03-12 00:45:57 +0600

    key commit

--- p-keywords.py
+++ p-keywords.py
@@ -23,10 +23,46 @@ for i in range(9):
     if i>3:
         break
         print(i)
-"""
+
 i=1
 while i<9:
     print(i)
     if i == 3:
         break
         i += 1
+
+class Person:
+    name :"Jhon"
+    age = 36
+
+p1 = Person()
+print(name)
+
+
+for i in range(9):
+    if i == 3:
+        continue
+        print(i)
+
+
+i=0
+while i<9:
+    i += 1
+    if i ==3:
+        continue
+        print(i)
+
+
+def my_function():
+    print("Hello from a function")
+my_function()
+
+
+x ="hello"
+del x
+print(x)
+"""
+
+x= ["apple","banana","cherry"]
+del x[0]
+print(x)
