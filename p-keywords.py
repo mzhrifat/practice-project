e8a3bd78d6eb5e272236dae470d506d64ee70d48
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2025-03-11 23:59:38 +0600

    p commit

---
+++ p-keywords.py
@@ -0,0 +1,32 @@
+"""
+x=(5>3 and 5<10)
+print(x)
+
+
+if 5>3 and 5<10:
+    print("Both statements are True")
+else:
+    print("At least one of the statements are False")
+
+
+import calendar as c
+print(c.month_name[6])
+
+
+x = "welcome"
+
+#if condition returns False, AssertionError is raised:
+assert x != "hello", "x should be 'hello'"
+
+
+for i in range(9):
+    if i>3:
+        break
+        print(i)
+"""
+i=1
+while i<9:
+    print(i)
+    if i == 3:
+        break
+        i += 1
