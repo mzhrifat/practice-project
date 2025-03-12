28ec907fc3564c2fc7872f8354143df4830d406e
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2025-03-12 17:02:31 +0600

    true false commit

---
+++ false to yield.py
@@ -0,0 +1,30 @@
+"""
+print (5>6)
+print(4 in [1,2,3])
+
+print ("hello" is "goodbye")
+print(5==6)
+print (5==6 or 6==7)
+print(5 ==6 and 6 ==7)
+print ("hello" is not "hello")
+print(not(5==5))
+print(3 not in [1,2,3])
+"""
+
+print(5 < 6)
+
+print(2 in [1,2,3])
+
+print(5 is 5)
+
+print(5 == 5)
+
+print(5 == 5 or 6 == 7)
+
+print(5 == 5 and 7 == 7)
+
+print("hello" is not "goodbye")
+
+print(not(5 == 7))
+
+print(4 not in [1,2,3])
