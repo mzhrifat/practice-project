1b96756a53c5337847a1a9703b10d4481135804e
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2025-03-14 23:20:05 +0600

    glossy 2 commit

--- p-Glosary.py
+++ p-Glosary.py
@@ -1,5 +1,28 @@
-
+"""
 if 5>2:
     print("five is greater than two!")
 if 5>2:
     print("Five  is greater than two!")
+
+
+x=5
+y="john"
+print(x)
+print(y)
+
+x=4
+x="salary"
+print(x)
+
+x="Jhon"
+x='kill'
+print(x)
+"""
+x="awesome"
+print("python is" , x)
+#string concreation
+
+a="hello"
+b="world"
+x=a+b
+print(x)
