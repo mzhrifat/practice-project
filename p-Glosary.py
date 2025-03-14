389714050c50edb9bdb0a6b6769d56a54cc0bf29
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2025-03-14 23:32:53 +0600

    glo commit

--- p-Glosary.py
+++ p-Glosary.py
@@ -17,7 +17,7 @@ print(x)
 x="Jhon"
 x='kill'
 print(x)
-"""
+
 x="awesome"
 print("python is" , x)
 #string concreation
@@ -26,3 +26,18 @@ a="hello"
 b="world"
 x=a+b
 print(x)
+"""
+#Global variables
+x="awesome"
+
+def myfunc():
+    print("python is" + x)
+myfunc()
+
+def myfunc():
+    global x
+    x="fantastic"
+
+myfunc()
+
+print("Python is "+ x)
