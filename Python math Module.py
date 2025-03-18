e609a536436dc001093b417ae2f38e4054083469
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2025-03-19 02:21:00 +0600

    cmath rect commit

--- Python math Module.py
+++ Python math Module.py
@@ -21,9 +21,17 @@ import math
 
 print(math.tau)
 
-"""
+
 #complex polar
 import cmath
 z = 1 + 1j
 result=cmath.polar(z)
+print(result)
+"""
+
+import cmath
+
+#from selenium.webdriver.common.devtools.v131.debugger import resume
+
+result=cmath.rect(1,cmath.pi/4)
 print(result)
