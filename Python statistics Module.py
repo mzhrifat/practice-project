9caaec9fb593843c557150f54fb22050f47b6ac5
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2025-03-18 23:58:18 +0600

    median commit

--- Python statistics Module.py
+++ Python statistics Module.py
@@ -6,12 +6,22 @@ import statistics
 
 print(statistics.harmonic_mean([40,60,80]))
 print(statistics.harmonic_mean([10,30,50,70,90]))
-"""
+
 from pandas.compat import is_platform_power
 
 #import statistics  Libray
+
 import statistics
+
 #calculate average valuse
 print (statistics.mean([1, 3, 5, 7, 9, 11, 13]))
 print(statistics.mean([1, 3, 5, 7, 9, 11,]))
 print(statistics.mean([-11,5.5,-3.4,7.1,-9,22]))
+
+"""
+import statistics
+#calculate the middle value
+print(statistics.median([1, 3, 5, 7, 9, 11, 13]))
+print(statistics.median([1, 3, 5, 7, 9, 11]))
+print(statistics.median([-11, 5.5, -3.4, 7.1, -9, 22]))
+
