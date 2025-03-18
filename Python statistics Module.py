d83c334846cdaac917fee11b494d88ae2d77c8e5
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2025-03-19 01:25:36 +0600

    mode commit

--- Python statistics Module.py
+++ Python statistics Module.py
@@ -24,7 +24,7 @@ import statistics
 print(statistics.median([1, 3, 5, 7, 9, 11, 13]))
 print(statistics.median([1, 3, 5, 7, 9, 11]))
 print(statistics.median([-11, 5.5, -3.4, 7.1, -9, 22]))
-"""
+
 import statistics
 #calculated the median of grouped continuous data
 
@@ -33,4 +33,10 @@ print(statistics.median_grouped([1, 2, 3, 4, 5]))
 print(statistics.median_grouped([1, 2, 3, 4], 2))
 print(statistics.median_grouped([1, 2, 3, 4], 3))
 print(statistics.median_grouped([1, 2, 3, 4], 5))
+"""
+import statistics
 
+#calculate the mode
+print(statistics.mode([1, 3, 3, 3, 5, 7, 7 ,9, 11]))
+print(statistics.mode([1, 1, 3, -5, 7, -9, 11]))
+print(statistics.mode(['red', 'green', 'blue', 'red']))
