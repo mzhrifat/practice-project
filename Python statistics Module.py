9a6e83b3434505a38770583d71ae7d7e5e631d6e
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2025-03-19 01:34:28 +0600

    variance commit

--- Python statistics Module.py
+++ Python statistics Module.py
@@ -60,7 +60,7 @@ print(statistics.stdev([1, 3, 5, 7, 9, 11]))
 print(statistics.stdev([2, 2.5, 1.25, 3.1, 1.75, 2.8]))
 print(statistics.stdev([-11, 5.5, -3.4, 7.1]))
 print(statistics.stdev([1, 30, 50, 100]))
-"""
+
 
 # Import statistics Library
 import statistics
@@ -70,3 +70,14 @@ print(statistics.pvariance([1, 3, 5, 7, 9, 11]))
 print(statistics.pvariance([2, 2.5, 1.25, 3.1, 1.75, 2.8]))
 print(statistics.pvariance([-11, 5.5, -3.4, 7.1]))
 print(statistics.pvariance([1, 30, 50, 100]))
+
+"""
+
+# Import statistics Library
+import statistics
+
+# Calculate the variance from a sample of data
+print(statistics.variance([1, 3, 5, 7, 9, 11]))
+print(statistics.variance([2, 2.5, 1.25, 3.1, 1.75, 2.8]))
+print(statistics.variance([-11, 5.5, -3.4, 7.1]))
+print(statistics.variance([1, 30, 50, 100]))
