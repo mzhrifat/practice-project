af66f8e73b6621b5795fcf8139e31559cc69516b
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2025-03-22 02:49:24 +0600

    del this tuple commit

--- python-example.py
+++ python-example.py
@@ -10,8 +10,20 @@ print(a.upper())
 #Lower
 b="MYNAME"
 print(b.lower())
-"""
+
 #replace
 c="HEllo, world"
 print(c.replace("E","I"))
+
+#MOdulas opeator
+
+x=46
+y=4
+print(x%y)
+"""
+#Delete this tuple
+
+thistuple=("apple","orange","mango")
+del thistuple
+print (thistuple)
 
