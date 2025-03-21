902cb9dbd0cad23075e40e2f4dde0e6c3d5c53d2
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2025-03-22 02:33:33 +0600

    reverse commit

--- remove-list-duplicates.py
+++ remove-list-duplicates.py
@@ -2,9 +2,21 @@
 mylist=["a","b","c","a","b","c"]
 mylist=list(dict.fromkeys(mylist))
 print(mylist)
-"""
+
 def my_function(x):
     return list(dict.fromkeys(x))
 
 mylist=my_function(["a","b","c","a","b","c"])
 print(mylist)
+"""
+#reverse a string in python
+
+txt="hello"[::-1]
+print(txt)
+
+def my_function(x):
+    return x[::-1]
+
+mytxt=my_function("I wonder how this text looks like backwards")
+
+print(mytxt)
