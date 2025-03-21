8f58ce45f088bae2939fea3bb8f3e40bda11b226
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2025-03-22 02:44:40 +0600

    example commit

---
+++ python-example.py
@@ -0,0 +1,17 @@
+#add all
+"""
+x= "python"
+y= "is"
+z= "awesome"
+print(x + y + z)
+#upper
+a="Hello"
+print(a.upper())
+#Lower
+b="MYNAME"
+print(b.lower())
+"""
+#replace
+c="HEllo, world"
+print(c.replace("E","I"))
+
