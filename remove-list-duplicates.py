61ed8b642c0b07992c4a5b4dc8bf73c4f79b367e
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2025-03-22 02:29:51 +0600

    def my_function commit

--- remove-list-duplicates.py
+++ remove-list-duplicates.py
@@ -1,5 +1,10 @@
-
+"""
 mylist=["a","b","c","a","b","c"]
 mylist=list(dict.fromkeys(mylist))
 print(mylist)
+"""
+def my_function(x):
+    return list(dict.fromkeys(x))
 
+mylist=my_function(["a","b","c","a","b","c"])
+print(mylist)
